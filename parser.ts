14595dc9e2462547ac80a8878791ad57ab1e6729
Nau077 <antbobrikov@yandex.ru>
2021-02-22 15:59:03 +0300

    add regex for hrefs

--- parser.ts
+++ parser.ts
@@ -6,11 +6,6 @@ const { JSDOM } = jsdom;
 var fs = require('fs');
 var fileName = __dirname + "/parse.html";
 
-// import { google } from "googleapis"
-// const key = "AIzaSyBv4HslhbU_GTDCHv7A5Ky9OKReeU4rdWU"
-// const search = google.customsearch("v1")
-// search.context._options.auth = key
-
 const parseAndSearch = async (searchString, site="") => {
     const googleUrl = `https://www.google.com/search?q=${searchString}${site ? `+site%3A${site}` : ""}`
     const sample = "https://www.npmjs.com/package/cheerio"
@@ -27,28 +22,24 @@ const parseAndSearch = async (searchString, site="") => {
        nodeArray.forEach( (a:HTMLDivElement) => {
          const children = Array.from(a.getElementsByClassName("kCrYT"))
          if (children[0] && children[1]) {
-           let parse_obj = {}
-   
-           if (children[0].querySelector("a") && children[0].querySelector("a").href) {
-             parse_obj["url"] = decodeURI(decodeURI(children[0].querySelector("a").href))
+          let parse_obj = {}
+          let url
+          if (children[0].querySelector("a") && children[0].querySelector("a").href) {
+              url = decodeURI(decodeURI(children[0].querySelector("a").href))
              parse_obj["title"] = children[0].getElementsByClassName("BNeawe vvjwJb AP7Wnd")[0].textContent
-             }
+            }
 
-             const description = children[1].getElementsByClassName("BNeawe s3v9rd AP7Wnd")[0] 
-   
-            // if (children[1].querySelector("a") && description && description.textContent && description.textContent.length) { 
-               parse_obj["description"] = children[1].textContent
-            // }
-   
-             parser_result.push(parse_obj)
+            if (!url) return 
+
+            parse_obj["url"] = url.match(/http(.+)/i)[0];
+            parse_obj["description"] = children[1].textContent
+             
+            parser_result.push(parse_obj)
            }
            
-           // console.log(children[1].getElementsByClassName("BNeawe s3v9rd AP7Wnd")[0].textContent)
          }
        )
-   
        return parser_result
-    
 }
 
 export default parseAndSearch
