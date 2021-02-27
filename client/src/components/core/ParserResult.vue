523498c9acb1f3baaeb84ea318d9e54ee8f063f5
Nau077 <antbobrikov@yandex.ru>
2021-02-27 21:38:13 +0300

    add parser result component

---
+++ client/src/components/core/ParserResult.vue
@@ -0,0 +1,14 @@
+ 
+<template>
+ <section class="container">
+		<article>
+			<h1>Hipsters</h1>
+
+			<p>You can&#8217;t dress up as a hipster for Halloween. Their attire is already so bizarre that there isn&#8217;t an
+			exaggeration of it that looks like a costume. It would just look like you are another hipster about to read a poem about reading poems.</p>
+
+			<h2>Secondary Title</h2>
+			<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+		</article>
+	</section>   
+</template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <FormMain/>
+    <ParserResult/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import FormMain from '@/components/core/FormMain.vue'
+import ParserResult from '@/components/core/ParserResult.vue'
 
 export default {
   name: 'Home',
   components: {
-    FormMain
+    FormMain,
+    ParserResult
   }
 }
 </script>
